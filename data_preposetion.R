50586e8c235d2e0df95077c3d839d0005b9a2fe3
Malysheva Sasha <malyshevasasha777@gmail.com>
2016-07-27 19:50:13 +0300

    Add machine learning

--- data_preposetion.R
+++ data_preposetion.R
@@ -1,38 +1,103 @@
-try http:// if https:// URLs are not supported 
-source("https://bioconductor.org/biocLite.R")
-biocLite("PAA")
-# add library for normalisation data
-biocLite("vsn")
-# add librery for Protein Microarray Data Analysis
-library("PAA")
-install.packages("Rcpp")
-
-library(limma)
 #Load GPR files
-my_data <- loadGPR(gpr.path="Data", targets.path="Data/targets.txt",array.type = "ProtoArray")
-# delete background
-bg_matrix <- backgroundCorrect(my_data, method="normexp", normexp.method="saddle")
+library(PAA)
+my_data <- loadGPR(
+  gpr.path="C:/Users/alex/alzhemerproject/Alzhemerproject/data",
+  targets.path = 
+    "C:/Users/alex/alzhemerproject/Alzhemerproject/data/targets.txt",
+  array.type = "ProtoArray");
 
 
-library("vsn")
-# normalize data
-plotMAPlots(elist=elist, idx=10)
-elist_1 <- normalizeArrays(elist=elist, method="cyclicloess", cyclicloess.method="fast")
+#plotArray(elist=my_data,  idx=3, data.type="bg", log=FALSE, normalized=FALSE,aggregation="min", colpal="topo.colors")
 
 
-library("pheatmap")
-# take matrix part
-E <- elist_1$E
-head(E)
+#Filtring
+library(limma)
+elist <- backgroundCorrect(my_data, method="normexp",
+                           normexp.method="saddle");
 
-# take names without trash
-gn = sub(pattern = 'Hs~', rep = "", x = elist_1$genes$Name)
+
+#Normalisation
+norm_data <- normalizeArrays(elist = elist, 
+                             method = "cyclicloess", cyclicloess.method = "fast");
+
+
+#Clear names
+name <- norm_data$genes$Name;
+gn = sub(pattern = 'Hs~', rep = "", x = name)
 gn = sub(pattern = '~.*', rep = "", gn)
 gn = sub(pattern = '.*:', rep = "", gn)
-head(gn)
 
-#add names to matrix
-row.names(E) <- gn
-aggr <- aggregate(E, list(gn), mean)
-row.names(aggr) <- aggr$Group.1
-aggr <- aggr[-1]
+
+#Naming
+row.names(norm_data$E) <- gn;
+
+
+#Meaning
+E <- norm_data$E
+mean_data <- aggregate(E, list(gn), mean);
+row.names(mean_data) <- mean_data$Group.1
+mean_data <- mean_data[-1];
+
+
+#P-value
+p_premult <- apply(mean_data,1,function(x) {t.test(x[1:50],x[51:90])$p.value});
+
+
+#false discovery rate multy normalisation
+adjusted <- p.adjust(p_premult, method = "fdr", n = length(p_premult));
+
+
+#Just low p-value
+adj <- sort(adjusted[adjusted < 10^-5]);
+
+
+#Choise samples vith low p-value
+re <- which(rownames(mean_data) %in% names(adj));
+er <- as.matrix(mean_data[re, ]);
+
+
+#hitmap for samples with minimum p-value
+library(pheatmap)
+heatmap(er, col = cm.colors(256))
+er<-t(er)
+
+
+#Убираем кариляцию
+library(caret)
+descrCor <-  cor(er)
+highlyCorDescr <- findCorrelation(descrCor, cutoff = .75)
+er <- er[,-highlyCorDescr]
+
+
+##Анализ главных компонент.
+my_pca=prcomp(er, scale=T, center=T)
+imp=summary(my_pca)$importance
+
+
+##Вносим принадлежность к группе. График каменистой осыпи
+barplot(imp[2,]*100,ylab="Persentage of variance", 
+        xlab="Principal Components",
+        main = "Variance explained by individual PC", col="cyan3")
+
+
+##Делаем таблицу данных из объекта - иначе его ggplot2 не распознает
+my_pca1 <- as.data.frame(my_pca$x)
+my_pca1$Group <- NA
+my_pca1[1:50,'Group'] <- 'AD'
+my_pca1[51:90,'Group'] <- 'NDC'
+
+
+##График главных компонент
+library(ggplot2)
+ggplot(my_pca1, aes(x = PC1, y = PC2, colour = Group)) + 
+  theme_bw(base_size = 8) + 
+  geom_point()+
+  labs(x = 'PC1', y = 'PC2') + 
+  scale_colour_manual(values = c("#00ba38","#d4170a","#00ebf2")) + 
+  theme(
+    legend.position = c(1, 0),
+    legend.direction = "vertical",
+    legend.justification = c(1, 0)
+  )+
+  stat_ellipse()
+
---
+++ heatMap.R
@@ -0,0 +1,41 @@
+try http:// if https:// URLs are not supported 
+source("https://bioconductor.org/biocLite.R")
+biocLite("PAA")
+# add library for normalisation data
+biocLite("vsn")
+# add librery for Protein Microarray Data Analysis
+library("PAA")
+install.packages("Rcpp")
+NUM_OF_AD = 50
+
+library(limma)
+#Load GPR files
+my_data <- loadGPR(gpr.path="Data", targets.path="Data/targets.txt",array.type = "ProtoArray")
+# delete background
+bg_matrix <- backgroundCorrect(my_data, method="normexp", normexp.method="saddle")
+
+
+library("vsn")
+# normalize data
+plotMAPlots(elist=elist, idx=10)
+elist_1 <- normalizeArrays(elist=elist, method="cyclicloess", cyclicloess.method="fast")
+
+
+library("pheatmap")
+# take matrix part
+E <- elist_1$E
+head(E)
+
+# take names without trash
+gn = sub(pattern = 'Hs~', rep = "", x = elist_1$genes$Name)
+gn = sub(pattern = '~.*', rep = "", gn)
+gn = sub(pattern = '.*:', rep = "", gn)
+head(gn)
+
+#add names to matrix
+row.names(E) <- gn
+
+library(pheatmap)
+heatmap()
+
+
